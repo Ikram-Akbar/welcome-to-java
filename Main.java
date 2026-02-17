85cf55505cdd877c12544fe1f1147bde40473c80
Ikram Akbar <ikramakbar71@gmail.com>
2026-02-17 22:33:38 +0600

    [check ]: run the project

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+  public static void main(String[] args) {
+    String name = "John";
+    System.out.println("Hello " + name);
+    System.out.println(5*5);
+    String brand = "new Coffee";
+    Integer count = 55;
+    Boolean isValid = false;
+    double newDigit = 55.25;
+    boolean isActive = true;
+  }
+}
